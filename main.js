cf7f44af82f8bcd23e3a0cc6710619049c816be7
Pedro <pedro.crawford@estudante.ufjf.br>
2023-11-28 22:27:15 -0300

    organiza funcao com origem

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 document.entrada.addEventListener('submit', leFormulario);
 
 function leFormulario(event) {
-event.preventDefault();
-const quantidade = document.entrada.quantidade.value;
-const fruta = document.entrada.fruta.value;
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.value;
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
 
-console.log(`Eu tenho ${quantidade} ${fruta}`);
+    console.log(`${origem} doa ${quantidade} ${fruta}`);
 }
