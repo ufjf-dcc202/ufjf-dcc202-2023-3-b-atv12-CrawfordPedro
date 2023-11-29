b32386e30905ba248bd7116b7a0a014558de6b4f
Pedro <pedro.crawford@estudante.ufjf.br>
2023-11-28 22:54:38 -0300

    adiciona pomar

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+const olJoao = document.querySelector("ol#joao");
+const olMaria = document.querySelector("ol#maria");
+const olPomar = document.querySelector("ol#pomar");
+
 document.entrada.addEventListener('submit', leFormulario);
 
 function leFormulario(event) {
     event.preventDefault();
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
     const fruta = document.entrada.fruta.value;
     const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
-    console.log(`${origem} doa ${quantidade} ${fruta}`);
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
