0c81ed30265b7574a86746509e08a31b7e299d1d
aaaaaaaaaaaa <aaaaaaaaaaa>
2023-12-05 23:11:20 -0300

    completa funcao main

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+import {getEstoque, transacao} from "./estoque.js";
+
 const olJoao = document.querySelector("ol#joao");
 const olMaria = document.querySelector("ol#maria");
 const olPomar = document.querySelector("ol#pomar");
 
 document.entrada.addEventListener('submit', leFormulario);
+
+atualizaTela();
 
 function leFormulario(event) {
     event.preventDefault();
@@ -12,4 +16,24 @@ function leFormulario(event) {
     const destino = document.entrada.destino.value;
 
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+
+    transacao(origem, destino, fruta, quantidade);
+    atualizaTela();
+}
+
+function atualizaTela() {
+    const estoque = getEstoque();
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
+    preencheLista(olPomar, estoque.pomar);
+}
+
+function preencheLista(lista, estoqueDaPessoa) {
+    lista.innerHTML = "";
+    for (let i=0; i<estoqueDaPessoa.length; i++) {
+        const monte = estoqueDaPessoa[i];
+        const li = document.createElement("li");
+        li.textContent = `${monte.tipo}: ${monte.qtd}`;
+        lista.append(li);
+    }
 }
